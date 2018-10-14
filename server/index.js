b6b704a4ca99f9158bdd02e1225d3c31857dd052
basedHokage <eugene.omar@andela.com>
2018-10-14 18:30:01 +0300

    Few changes so I can commit some stuff to heroku

--- server/index.js
+++ server/index.js
@@ -1,1 +1,11 @@
 const express = require('express')
+const app = express()
+
+app.get('*', (req, res) =>
+  res.status(200).send({
+    message: 'Welcome to the beginning of nothingness.'
+  })
+)
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT)
